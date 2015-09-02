4435b6488dd08d7e1242ed9628847e666b2bf730
cospan <cospan@gmail.com>
2015-09-02 10:47:38 -0400

    Initial version of sdio-device

---
+++ rtl/sdio_device_stack.v
@@ -0,0 +1,263 @@
+/*
+Copyright (c) 2015 Dave McCoy (dave.mccoy@cospandesign.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ * Author: David McCoy
+ *
+ * NOTE: Add Pullups to all the signals, this will tell the host we are
+ *    an SD Device, not a SPI Device
+ * Description: SDIO Stack
+ *  Manages the entire SDIO communication flow, in the end users should
+ *  Write into the stack and it should arrive on the other side.
+ *
+ *  Data Link Layer:
+ *    Sends and receive commands and responses from the physical layer.
+ *    Manages all register read and writes that will be used to configure
+ *    the entire stack.
+ *
+ *  Phy Layer:
+ *    Sends and receives streams of bits with the host and manages CRC
+ *    generation and analysis. The bottom part of this layer is connected
+ *    to the physical pins of the FPGA and the top is connected to the data
+ *    link layer
+ *
+ * Changes:
+ *  2015.08.09: Inital Commit
+ */
+
+module sdio_device_stack (
+
+  input               sdio_clk,
+  input               rst,
+
+  //Function Configuration
+  output  [15:0]      o_block_size,           //Maximum Size of block transfer
+  output  [7:0]       o_func_enable,          //Bitmask Function Enable
+  input   [7:0]       i_func_ready,           //Bitmask Function is Ready
+  output  [2:0]       o_func_abort,
+  output  [7:0]       o_func_int_en,
+  input   [7:0]       i_func_int_pending,
+  input   [7:0]       i_func_exec_status,
+
+  // Function Interface From CIA
+  output              o_fbr1_csa_en,
+  output  [3:0]       o_fbr1_pwr_mode,
+  output  [15:0]      o_fbr1_block_size,
+
+  output              o_fbr2_csa_en,
+  output  [3:0]       o_fbr2_pwr_mode,
+  output  [15:0]      o_fbr2_block_size,
+
+  output              o_fbr3_csa_en,
+  output  [3:0]       o_fbr3_pwr_mode,
+  output  [15:0]      o_fbr3_block_size,
+
+  output              o_fbr4_csa_en,
+  output  [3:0]       o_fbr4_pwr_mode,
+  output  [15:0]      o_fbr4_block_size,
+
+  output              o_fbr5_csa_en,
+  output  [3:0]       o_fbr5_pwr_mode,
+  output  [15:0]      o_fbr5_block_size,
+
+  output              o_fbr6_csa_en,
+  output  [3:0]       o_fbr6_pwr_mode,
+  output  [15:0]      o_fbr6_block_size,
+
+  output              o_fbr7_csa_en,
+  output  [3:0]       o_fbr7_pwr_mode,
+  output  [15:0]      o_fbr7_block_size,
+
+
+  //Function Bus
+  output              o_func_activate,
+  input               i_func_finished,
+
+  output              o_func_inc_addr,
+  output              o_func_block_mode,
+
+  output  [3:0]       o_func_num,
+  output              o_func_write_flag,
+  output              o_func_rd_after_wr,
+  output  [17:0]      o_func_addr,
+  output  [7:0]       o_func_write_data,
+  input   [7:0]       i_func_read_data,
+  input               o_func_data_rdy,
+  output              i_func_host_rdy,
+  output  [17:0]      o_func_data_count,
+
+  input   [7:0]       i_interrupt,
+
+  //FPGA Interface
+  input               i_sdio_clk,
+  inout               io_sdio_cmd,
+  inout   [3:0]       io_sdio_data
+
+);
+
+//local parameters
+//registes/wires
+wire        [3:0]   sdio_state;
+wire                sdio_cmd_in;
+wire                sdio_cmd_out;
+wire                sdio_cmd_dir;
+
+wire        [3:0]   sdio_data_in;
+wire        [3:0]   sdio_data_out;
+wire                sdio_data_dir;
+
+//Phy Configuration
+wire                spi_phy;
+wire                sd1_phy;
+wire                sd4_phy;
+
+//Phy Interface
+wire                cmd_phy_idle;
+wire                cmd_stb;
+wire                cmd_crc_stb;
+wire        [5:0]   cmd;
+wire        [31:0]  cmd_arg;
+
+wire        [39:0]  rsps;
+wire        [7:0]   rsps_len;
+
+wire                interrupt;
+wire                read_wait;
+wire                chip_select_n;
+
+//Function Level
+wire                func_activate;
+wire                func_inc_addr;
+wire                func_block_mode;
+
+wire                tunning_block;
+
+
+//Submodules
+sdio_card_control card_controller (
+  .sdio_clk                 (sdio_clk               ),/* Run from the SDIO Clock */
+  .rst                      (rst                    ),
+
+  .o_func_activate          (func_activate          ),/* CMD -> FUNC: Start a function layer transaction */
+  .i_func_finished          (func_finished          ),/* FUNC -> CMD: Function has finished */
+  .o_func_inc_addr          (func_inc_addr          ),/* CMD -> FUNC: Increment the address after every read/write */
+  .o_func_block_mode        (func_block_mode        ),/* CMD -> FUNC: This is a block level transfer, not byte */
+
+  .o_func_num               (o_func_num             ),/* CMD -> FUNC: Function Number to activate */
+  .o_func_write_flag        (o_func_write_flag      ),/* CMD -> FUNC: We are writing */
+  .o_func_rd_after_wr       (o_func_rd_after_wr     ),/* CMD -> FUNC: Read the value after a write */
+  .o_func_addr              (o_func_addr            ),/* CMD -> FUNC: Address we are talking to */
+  .o_func_write_data        (o_func_write_data      ),/* CMD -> FUNC: Data to Write */
+  .i_func_read_data         (i_func_read_data       ),/* FUNC -> CMD: Read Data */
+  .i_func_data_rdy          (i_func_data_rdy        ),/* FUNC -> CMD: Function is ready for write data */
+  .o_func_host_rdy          (o_func_host_rdy        ),/* CMD -> FUNC: Host is ready to read data */
+  .o_func_data_count        (o_func_data_count      ),/* CMD -> FUNC: number of data bytes/blocks to read/write */
+
+  .o_tunning_block          (tunning_block          ),
+
+  // Function Interface From CIA
+  .o_fbr1_csa_en            (o_fbr1_csa_en          ),
+  .o_fbr1_pwr_mode          (o_fbr1_pwr_mode        ),
+  .o_fbr1_block_size        (o_fbr1_block_size      ),
+
+  .o_fbr2_csa_en            (o_fbr2_csa_en          ),
+  .o_fbr2_pwr_mode          (o_fbr2_pwr_mode        ),
+  .o_fbr2_block_size        (o_fbr2_block_size      ),
+
+  .o_fbr3_csa_en            (o_fbr3_csa_en          ),
+  .o_fbr3_pwr_mode          (o_fbr3_pwr_mode        ),
+  .o_fbr3_block_size        (o_fbr3_block_size      ),
+
+  .o_fbr4_csa_en            (o_fbr4_csa_en          ),
+  .o_fbr4_pwr_mode          (o_fbr4_pwr_mode        ),
+  .o_fbr4_block_size        (o_fbr4_block_size      ),
+
+  .o_fbr5_csa_en            (o_fbr5_csa_en          ),
+  .o_fbr5_pwr_mode          (o_fbr5_pwr_mode        ),
+  .o_fbr5_block_size        (o_fbr5_block_size      ),
+
+  .o_fbr6_csa_en            (o_fbr6_csa_en          ),
+  .o_fbr6_pwr_mode          (o_fbr6_pwr_mode        ),
+  .o_fbr6_block_size        (o_fbr6_block_size      ),
+
+  .o_fbr7_csa_en            (o_fbr7_csa_en          ),
+  .o_fbr7_pwr_mode          (o_fbr7_pwr_mode        ),
+  .o_fbr7_block_size        (o_fbr7_block_size      ),
+
+  .i_cmd_phy_idle           (cmd_phy_idle           ),/* PHY -> CMD: Command portion of phy layer is IDLE */
+  .i_cmd_stb                (cmd_stb                ),/* PHY -> CMD: Command signal strobe */
+  .i_cmd_crc_good_stb       (cmd_crc_good_stb       ),/* PHY -> CMD: CRC is good */
+  .i_cmd                    (cmd                    ),/* PHY -> CMD: Command */
+  .i_cmd_arg                (cmd_arg                ),/* PHY -> CMD: Command Arg */
+
+  .i_chip_select_n          (chip_select_n          ),/* Chip Select used to determine if this is a SPI host */
+
+  .o_rsps                   (rsps                   ),/* Response Generated by this layer*/
+  .o_rsps_len               (rsps_len               ),/* Length of response*/
+  .o_rsps_stb               (rsps_stb               )
+);
+
+sdio_device_phy phy(
+  .rst                      (rst                    ),
+
+  //Configuration
+  .spi_phy                  (spi_phy                ),/* Flag: SPI PHY (not supported now) */
+  .sd1_phy                  (sd1_phy                ),/* Flag: SD  PHY with one data lane */
+  .sd4_phy                  (sd4_phy                ),/* Flag: SD  PHY with four data lanes */
+
+  .cmd_phy_idle             (cmd_phy_idle           ),/* PHY -> CMD: Command portion of phy layer is IDLE */
+
+  //Data Link Interface
+  .cmd_stb                  (cmd_stb                ),/* PHY -> CMD: Command signal strobe */
+  .cmd_crc_good_stb         (cmd_crc_good_stb       ),/* PHY -> CMD: CRC is good */
+  .cmd                      (cmd                    ),/* PHY -> CMD: Command */
+  .cmd_arg                  (cmd_arg                ),/* PHY -> CMD: Command Arg */
+
+  .rsps                     (rsps                   ),/* CMD -> PHY: Response Value */
+  .rsps_len                 (rsps_len               ),/* CMD -> PHY: Response Length */
+
+  .interrupt                (interrupt              ),/* Interrupt */
+  .read_wait                (read_wait              ),/* SDIO Device is busy working on generated a read */
+
+  //FPGA Interface
+  .ddr_en                   (ddr_en                 ),
+  .sdio_clk                 (sdio_clk               ),
+  .sdio_cmd_in              (sdio_cmd_in            ),
+  .sdio_cmd_out             (sdio_cmd_out           ),
+  .sdio_cmd_dir             (sdio_cmd_dir           ),
+  .sdio_data_in             (sdio_data_in           ),
+  .sdio_data_out            (sdio_data_out          ),
+  .sdio_data_dir            (sdio_data_dir          )
+
+);
+
+
+//asynchronous logic
+/*
+assign  sdio_cmd      = sdio_cmd_dir  ? sdio_cmd_out  : sdio_cmd_in;
+assign  sdio_data     = sdio_data_dir ? sdio_data_out : sdio_data_in;
+assign  chip_select_n = sdio_data[3];
+*/
+
+//synchronous logic
+
+endmodule
---
+++ sim/demo_function.v
@@ -0,0 +1,265 @@
+/*
+Distributed under the MIT license.
+Copyright (c) 2015 Dave McCoy (dave.mccoy@cospandesign.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ * Author: Demo Function
+ * Description: Demonstrate all possible characteristics of a function
+ *  including
+ *      Single Byte Write
+ *      Single Byte Read
+ *      Multiple Byte Write
+ *      Multiple Byte Read
+ *      Block Write
+ *      Block Read
+ *      Incrementing Address Write
+ *      Non Incrementing Address Write
+ *      Incrementing Address Read
+ *      Non Incrementing Address Read
+ *
+ *      Test Interrupt
+ *          Enable
+ *          Interrupt Action
+ *          Interrupt Pending
+ *
+ *      Read Wait
+ *          System Response to a read wait
+ *
+ *  Note: All function configuration needs to be done inside
+ *      sdio_configuration.json file
+ *
+ *  Parameters:
+ *      FUN_NUM: The function index to respond to, e.g. 1 for function 1
+ *
+ * Changes:
+ */
+
+`include "sdio_cia_defines.v"
+
+module demo_function #(
+  parameter                 FUNC_NUM = 1
+)(
+  input                     clk,
+  input                     sdio_clk,
+  input                     rst,
+
+  //Configuration
+  input                     i_csa_en,
+  input                     i_pwr_mode,
+  input         [15:0]      i_block_size,           //Maximum Size of block transfer
+  input                     i_enable,               //Host says they want to use the function
+  output                    o_ready,                //Function is ready to accept data/command
+  input                     i_abort,                //Host asks to abort a transaction
+  input                     i_interrupt_enable,     //Enable generating an interrupt from this func
+  output                    o_interrupt_pending,    //An interrupt fired off
+  output                    o_busy,                 //Current status of the function
+
+  //Transaction Configuration
+  input                     i_activate,             //Activate for a data transfer
+  output    reg             o_finished,             //Function asserts this signal when it is finished
+  input                     i_inc_addr,             //Function will increment address for each conecutive r/w
+  input                     i_block_mode,           //0 = Transfering bytes at a time, 1 = transfering at a block
+
+  //Transaction
+  input                     i_write_flag,           //Read = 0, Write = 1
+  input                     i_rd_after_wr,          //Read the value after write, only valid for 1 byte
+  input         [17:0]      i_addr,                 //Address within Function
+  input         [7:0]       i_write_data,           //Data to write
+  output    reg [7:0]       o_read_data,            //Data to read
+  output    reg             o_data_rdy,             //Function is ready to receive up to block size of data
+  input                     i_data_stb,             //Host is sending a byte of data
+  input                     i_host_rdy,             //Host is ready to receive data from function
+  input         [17:0]      i_data_count,           //Number of bytes or blocks to read/write
+  output    reg             o_data_stb,             //SDIO sends a piece of data to host
+
+  //Out of band signaling
+  output                    o_read_wait,            //When asserted function is not ready to receive more data
+  output                    o_interrupt,            //Launch off an interrupt
+
+  //Test Stimulate
+  input                     i_request_read_wait,    //Used for Simulation to test read wait
+  input                     i_request_interrupt     //Used for simulation to test interrupt
+
+);
+//local parameters
+localparam      IDLE        =   4'h0;
+localparam      WRITE       =   4'h1;
+localparam      READ        =   4'h2;
+localparam      FINISHED    =   4'h5;
+
+//registes/wires
+reg             [3:0]       state;
+wire            [11:0]      mem_addr;
+reg                         mem_write_stb;
+reg             [31:0]      mem_write_data;
+wire            [31:0]      mem_read_data;
+reg             [17:0]      data_count;
+reg             [1:0]       byte_count;
+reg             [31:0]      block_data_count;
+wire                        count_finished;
+reg             [31:0]      address;
+
+//submodules
+blk_mem #(
+    .DATA_WIDTH             (32             ),  //More Challenging but this is going to be more like final applications
+    .ADDRESS_WIDTH          (12             ),  //4096
+    .INC_NUM_PATTERN        (1              )   //Incrementing data pattern to read and modify
+)mem(
+    .clka                   (sdio_clk       ),
+    .wea                    (mem_write_stb  ),
+    .addra                  (mem_addr       ),
+    .clkb                   (blk            ),
+    .addrb                  (mem_addr       ),
+    .doutb                  (mem_read_data  )
+);
+//asynchronous logic
+assign  mem_addr            =   address[11:0];
+assign  o_busy              =   state != IDLE;
+assign  count_finished      =   i_block_mode ?  (block_data_count >= i_data_count) :
+                                                (data_count       >= i_data_count);
+assign  o_read_wait         =   (`SRW && i_request_read_wait);
+assign  o_interrupt_pending =   (i_interrupt_enable && i_request_interrupt);
+assign  o_interrupt         =   o_interrupt_pending;
+
+//synchronous logic
+always @ (*) begin
+  if(rst) begin
+    o_read_data             <=  8'h0;
+  end
+  else begin
+    case (byte_count)
+      2'b00:
+        o_read_data         <=  mem_read_data[7:0];
+      2'b01:
+        o_read_data         <=  mem_read_data[15:8];
+      2'b10:
+        o_read_data         <=  mem_read_data[23:16];
+      2'b11:
+        o_read_data         <=  mem_read_data[31:24];
+    endcase
+  end
+end
+
+//Counter
+//XXX: This may need to be a mealy state machine
+always @ (posedge sdio_clk) begin
+  if (rst) begin
+    data_count              <=  0;
+    block_data_count        <=  0;
+    address                 <=  0;
+  end
+  else begin
+    if (state == IDLE) begin
+        data_count          <=  0;
+        block_data_count    <=  0;
+        address             <=  i_addr[15:4];
+    end
+    else if ((state == WRITE) || (state == READ)) begin
+      if (i_data_stb || o_data_stb) begin
+        if (i_inc_addr) begin
+          address           <=  address + 1;
+        end
+        if (i_block_mode) begin
+          if (data_count < i_block_size) begin
+            data_count      <=  data_count + 18'h1;
+          end
+          else begin
+            if (block_data_count  < data_count) begin
+              block_data_count    <=  block_data_count + 1;
+              data_count  <=  18'h0;
+            end
+          end
+        end
+        else begin
+          if (data_count < i_data_count) begin
+              data_count  <=  data_count + 18'h1;
+          end
+        end
+      end
+    end
+  end
+end
+
+//Main State Machine
+always @ (posedge sdio_clk) begin
+  mem_write_stb             <=  0;
+  o_data_stb                <=  0;
+  if (rst) begin
+    byte_count              <=  0;
+    mem_write_data          <=  0;
+    o_finished              <=  0;
+    o_data_rdy              <=  0;
+
+    state                   <=  IDLE;
+  end
+  else begin
+    case (state)
+      IDLE: begin
+        byte_count          <=  0;
+        o_data_rdy          <=  0;
+        if (i_activate)begin
+          o_data_rdy        <=  1;
+          if (i_write_flag) begin
+            state           <=  WRITE;
+          end
+          else begin
+            state           <=  READ;
+          end
+        end
+      end
+      WRITE: begin
+        if (count_finished) begin
+          state             <= FINISHED;
+        end
+        if (i_data_stb) begin
+          //Shift Data In
+          mem_write_data      <=  {mem_write_data[23:0], i_write_data};
+          if (byte_count == 2'b11) begin
+            mem_write_stb     <=  1;
+          end
+          byte_count          <= byte_count + 2'b01;
+        end
+      end
+      READ: begin
+        if (count_finished) begin
+          state             <= FINISHED;
+        end
+        if (i_host_rdy) begin
+          o_data_stb        <= 1;
+          byte_count        <=  byte_count + 2'b01;
+        end
+      end
+      FINISHED: begin
+        o_data_rdy          <=  0;
+        if (!i_activate) begin
+          state             <=  IDLE;
+        end
+      end
+      default: begin
+          state             <=  IDLE;
+      end
+    endcase
+  end
+end
+
+
+endmodule
